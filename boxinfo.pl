54100250edaf939424e1a7c8fd9ab4aaa00add6b
Greg Sabino Mullane <greg@endpoint.com>
2010-04-29 13:16:34 -0400

    Bump version to 1.2.0, get ready for release.

--- boxinfo.pl
+++ boxinfo.pl
@@ -15,7 +15,7 @@ use Data::Dumper   qw{ Dumper     };
 use Getopt::Long   qw{ GetOptions };
 use File::Basename qw{ basename   };
 
-our $VERSION = '1.1.13';
+our $VERSION = '1.2.0';
 
 my $USAGE = "Usage: $0 <options>
  Important options:
