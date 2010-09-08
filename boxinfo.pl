bef9f542c885a5a4e4c06ef1d80a89134096c9f2
Greg Sabino Mullane <greg@endpoint.com>
2010-09-08 16:23:22 -0400

    Version bump

--- boxinfo.pl
+++ boxinfo.pl
@@ -16,7 +16,7 @@ use Data::Dumper   qw{ Dumper     };
 use Getopt::Long   qw{ GetOptions };
 use File::Basename qw{ basename   };
 
-our $VERSION = '1.3.1';
+our $VERSION = '1.3.2';
 
 my $USAGE = "Usage: $0 <options>
  Important options:
